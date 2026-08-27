88ca961c1f3c00f748ac93ef82c6a55c9ed6aeb0
Ahmad Hijazi <152534231+ahmxdhijazi@users.noreply.github.com>
2026-08-27 16:47:44 -0500

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.py
@@ -0,0 +1,19 @@
+import math
+class Solution:
+    #using quicksort with the given points, we can sort the distances into an array and return k closest to origin
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #Nieve solving: we sort the distances and just return k elements
+        distance_point_pairs = []
+        for x, y in points:
+            distance = self.getDistance(x, y)
+            distance_point_pairs.append((distance, [x, y]))
+
+        distance_point_pairs.sort()
+
+        return [point for dist, point in distance_point_pairs[:k]]
+    
+    def getDistance(self, x1, y1) -> float:
+        distance = math.sqrt((x1 - 0)**2 + (y1 - 0)**2)
+        return distance
+
+
