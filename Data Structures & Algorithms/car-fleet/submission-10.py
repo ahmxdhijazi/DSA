dd3d674bbf3bbb09b780f5b933cca21c2e5f09d0
Ahmad Hijazi <152534231+ahmxdhijazi@users.noreply.github.com>
2026-09-10 17:32:58 -0500

    Add: car-fleet - submission-10

---
+++ Data Structures & Algorithms/car-fleet/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[pos,spd] for pos, spd in zip(position, speed)]
+        stack = [] #results
+
+        #Pair looks like [(pos, speed), (pos, speed)..... length n]
+        #So now we have an array with tuples of the position and speeds
+
+        #Sort them by the first index and go through it in reverse
+        for pos, spd in sorted(pair)[::-1]:
+            time = (target-pos)/spd
+            stack.append(time)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        
+        return len(stack)
